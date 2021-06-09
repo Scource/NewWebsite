7970ef545b87d7893bcc789e85255133b7f7fa84
Scource <tomekwa@gmail.com>
2021-06-09 10:11:42 +0200

    added starting version

---
+++ BMsite/BMusers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BMusersConfig(AppConfig):
+    name = 'BMusers'
---
+++ BMsite/BMusers/urls.py
@@ -0,0 +1,33 @@
+"""
+ BMusers urls
+"""
+
+from django.urls import path
+import BMusers.views as views
+
+
+app_name = 'BMusers'
+urlpatterns = [
+    path('connection/', views.ConnList.as_view()),
+    path('connection/create/', views.ConnCreateView.as_view()),
+    path('connection/<int:pk>/edit/', views.ConnUpdateView.as_view()),
+    path('connection/<int:pk>/delete/', views.ConnDeleteView.as_view()),
+
+    path('element/<str:type>/', views.ElementList.as_view()),
+    path('element/create/', views.ElementCreateView.as_view()),
+    path('element/<int:pk>/edit/', views.ElementUpdateView.as_view()),
+    path('element/<int:pk>/delete/', views.ElementDeleteView.as_view()),
+
+    path('powerplant/', views.PowerPlantList.as_view()),
+    path('powerplant/create/', views.PowerPlantCreateView.as_view()),
+    path('powerplant/<int:pk>/edit/', views.PowerPlantUpdateView.as_view()),
+    path('powerplant/<int:pk>/delete/', views.PowerPlantDeleteView.as_view()),
+
+
+    path('powerplant/connection/<int:ppid>/',
+         views.PowerPlantConnectionList.as_view()),
+    path('powerplant/connection/create/',
+         views.PowerPlantConnectionCreateView.as_view()),
+    path('powerplant/connection/<int:pk>/edit/',
+         views.PowerPlantConnectionUpdateView.as_view()),
+]
